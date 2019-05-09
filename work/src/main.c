bc9d249dd3ff3d8285342ff82a1e5c3f3272cd1d
vesofton <50405194+vesofton@users.noreply.github.com>
2019-05-09 13:03:24 +0200

    Update main.c
    Code opgeschoond

--- work/src/main.c
+++ work/src/main.c
@@ -3,7 +3,7 @@
 // Datum    : 30.03.2016
 // Version  : 1.0
 // Autor    : UB
-// mods by	: J.F. van der Bent
+// mods by  : J.F. van der Bent
 // CPU      : STM32F4
 // IDE      : CooCox CoIDE 1.7.x
 // Module   : CMSIS_BOOT, M4_CMSIS_CORE
@@ -14,23 +14,17 @@
 #include "stm32_ub_vga_screen.h"
 #include <math.h>
 
-int main(void)
+int main (void)
 {
+    //  uint32_t n;
+    SystemInit(); // System speed to 168MHz
 
-	//  uint32_t n;
-
-	SystemInit(); // System speed to 168MHz
-
-	UB_VGA_Screen_Init(); // Init VGA-Screen
-
+    UB_VGA_Screen_Init(); // Init VGA-Screen
     UB_VGA_FillScreen(VGA_COL_CYAN);
     UB_VGA_SetPixel(10,10,10);
 
-
-
-
-  while(1)
-  {
-  }
+    while(1)
+    {
+    }
 }
 
