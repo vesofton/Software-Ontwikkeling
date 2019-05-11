02228add9d740b11b530b878abb15c8c2a0f39e7
shippable <user@shippable.com>
2019-05-11 21:31:52 +0200

    Updated Doxygen generation files

--- deploy_to_github.sh
+++ deploy_to_github.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # -------------------------------------------------
-# Title		: deploy_to_github.sh
-# Date Created	: 11-05-2019
-# Author	: Henrico Brom
+# Title         : deploy_to_github.sh
+# Date Created  : 11-05-2019
+# Author        : Henrico Brom
 # -------------------------------------------------
 
 
@@ -11,17 +11,18 @@ git config user.name "shippable"
 git config user.email "user@shippable.com"
 
 
-echo "Test"
-if [ "git ls-remote --heads git@github.com:vesofton/Software-Ontwikkeling.git doxygen_output | wc -l" = "1" ]; then
-	echo "Creating branch 'doxygen_output'" 
-	git checkout -b doxygen_output
-	git push -u origin doxygen_output
-	rm -rf *
+BRANCH_EXIST=$(git ls-remote --heads git@github.com:vesofton/Software-Ontwikkeling.git doxygen_output | wc -l)
+if [ $BRANCH_EXIST = 0 ]; then
+        echo "Creating branch 'doxygen_output'"
+        git checkout --orphan doxygen_output
+	git clean -fdx
 fi
 
+rm -rf *
+cp -R $DEPLOY_DIR/. .
 
 # Track all modified files
-git add output
+git add .
 
 # You must commit with message containing '[skip ci]' to prevent
 # invoking another build (and creating an infinite loop)
@@ -29,3 +30,4 @@ git commit -m "Deploy Doxygen files to GitHub [skip ci]"
 
 # In order to push to GitHub, the Deploymeny key must be set
 git push git@github.com:vesofton/Software-Ontwikkeling.git doxygen_output
+
