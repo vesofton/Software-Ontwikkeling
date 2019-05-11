9afc853cd61419b919ead3f8c2d62e23672bbe18
shippable <user@shippable.com>
2019-05-11 22:02:19 +0200

    Added comments to deploy_to_github.sh file

--- deploy_to_github.sh
+++ deploy_to_github.sh
@@ -7,10 +7,13 @@
 # -------------------------------------------------
 
 
+# Configure git with a standard name and email
 git config user.name "shippable"
 git config user.email "user@shippable.com"
 
 
+# Check if the branch 'doxygen_output' exist,
+# if not create it else do a checkout
 BRANCH_EXIST=$(git ls-remote --heads git@github.com:$DEPLOY_USERNAME/$DEPLOY_REPONAME.git doxygen_output | wc -l)
 if [ $BRANCH_EXIST = 0 ]; then
         echo "Creating branch 'doxygen_output'"
@@ -20,16 +23,23 @@ else
 	git checkout doxygen_output
 fi
 
+
+# Remove all existing files and 
+# copy the newly genereted doxygen files into the current directory
 rm -rf *
 cp -R $DEPLOY_DIR/. .
+
 
 # Track all modified files
 git add .
 
-# You must commit with message containing '[skip ci]' to prevent
+
+# Commit with a message containing '[skip ci]' to prevent
 # invoking another build (and creating an infinite loop)
 git commit -m "Deploy Doxygen files to GitHub [skip ci]"
 
-# In order to push to GitHub, the Deploymeny key must be set
+
+# Push the commited files to GitHub,
+# this will only work when the Deployment Key is set in GitHub
 git push git@github.com:$DEPLOY_USERNAME/$DEPLOY_REPONAME.git doxygen_output
 
