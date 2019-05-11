49c3408e3ad70d487289f2e00736666081adb26d
henbro12 <henricobrom@gmail.com>
2019-05-11 18:13:09 +0200

    Updated 'deploy_to_github.sh

--- deploy_to_github.sh
+++ deploy_to_github.sh
@@ -10,9 +10,12 @@
 git config user.name "shippable"
 git config user.email "user@shippable.com"
 
+# Track all modified files
+git add .
+
 # You must commit with message containing '[skip ci]' to prevent
 # invoking another build (and creating an infinite loop)
-git commit output -m "Deploy Doxygen files to GitHub [skip ci]"
+git commit -m "Deploy Doxygen files to GitHub [skip ci]"
 
 # In order to push to GitHub, the Deploymeny key must be set
 git push git@github.com:vesofton/Software-Ontwikkeling.git doxygen
